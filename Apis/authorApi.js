8f0039608c116ea3373a46296aba8c46e07e079e
Bhargavikatike <23eg105e31@anurag.edu.in>
2026-05-13 09:56:54 +0530

    fixed case-sensitive import paths

--- Apis/authorApi.js
+++ Apis/authorApi.js
@@ -1,6 +1,6 @@
 import exp from "express";
 import { register } from "../Services/authService.js";
-import { ArticleModel } from "../models/ArticleModel.js";
+import { ArticleModel } from "../Models/articleModel.js";
 import { verifyToken } from "../middlewares/verifyToken.js";
 import { upload } from "../config/multer.js";
 import cloudinary from "../config/cloudinary.js";
